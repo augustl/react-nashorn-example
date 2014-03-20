340a6161ca0c4d438933240bf1cde89376e2a745
August Lilleaas <august@augustl.com>
2014-03-20 10:51:54 +0100

    Dem comments.

--- src/react_nashorn_example/nashorn_utils.clj
+++ src/react_nashorn_example/nashorn_utils.clj
@@ -4,18 +4,27 @@
             ScriptContext]))
 
 (defn bindings-append
+  "'inherits' bindings, allowing you to eval a script with extra global variables set, and
+    also inheriting all the existing global variables.
+
+    (.eval nashorn \"var hmm = 2;\")
+    (.eval nashorn \"hmm + 2\" (.createBindings nashorn)) ;; An error, 'hmm' is not defined.
+    (.eval nashorn \"hmm + wat\" (bindings-append nashorn {\"wat\" 3})) ;; 5"
   [nashorn new-bindings]
   (let [bindings (.createBindings nashorn)]
+    ;; Add our own bindings
     (doseq [[key value] new-bindings]
       (.put bindings key value))
+    ;; Inherit global bindings
     (.putAll bindings (.getBindings nashorn ScriptContext/ENGINE_SCOPE))
     bindings))
 
 (defn create-engine
-  [assets]
+  "Creates a new nashorn script engine and loads a bunch of scripts into it."
+  [scripts]
   (let [nashorn (.getEngineByName (ScriptEngineManager.) "nashorn")]
     ;; Browser module shims expects either 'window' or 'global' to be around.
     (.eval nashorn "var global = this")
-    ;; Assets is a list of strings or readables, load them all into nashorn.
-    (doseq [asset assets] (.eval nashorn asset))
+    ;; 'scripts' is a list of strings or readables, load them all into nashorn.
+    (doseq [script scripts] (.eval nashorn script))
     nashorn))
