eea4749b6e52f47359a173267b59de1f8d81b996
August Lilleaas <august@augustl.com>
2014-03-20 21:00:25 +0100

    It's really a generic obj to map function.

--- resources/public/js/run-nashorn.js
+++ resources/public/js/run-nashorn.js
@@ -4,19 +4,19 @@
     var app = MyApp.create();
     var apiFetcher = Java.type('react_nashorn_example.js_api_fetcher')
 
-    function urlsToHashMap(urls) {
+    function mapToHashMap(map) {
         var HashMap = Java.type("java.util.HashMap");
         var result = new HashMap();
-        for (var prop in urls) {
-            if (urls.hasOwnProperty(prop)) {
-                result.put(prop, urls[prop]);
+        for (var prop in map) {
+            if (map.hasOwnProperty(prop)) {
+                result.put(prop, map[prop]);
             }
         }
         return result;
     }
     function getProps(urls) {
         if (urls) {
-            var res = apiFetcher.resolveUrls(urlsToHashMap(urls));
+            var res = apiFetcher.resolveUrls(mapToHashMap(urls));
             var parsedRes = {};
             for (var prop in res) {
                 parsedRes[prop] = JSON.parse(res[prop])
