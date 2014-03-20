1cccad49108add4cefe9063eae0cd9e126a373bf
August Lilleaas <august@augustl.com>
2014-03-20 21:08:31 +0100

    Saving some more lines.

--- resources/public/js/run-browser.js
+++ resources/public/js/run-browser.js
@@ -10,59 +10,42 @@
         renderComponent(app.getNotFoundComponent());
     }
 
-    function getUrl(prop, url) {
+    function getUrl(url) {
         var deferred = when.defer();
         var req = new XMLHttpRequest();
         req.open("GET", url, true);
         req.send();
         req.onload = function () {
             if (req.status >= 200 && req.status < 300) {
-                deferred.resolve({prop: prop, url: url, body: req.responseText});
+                deferred.resolve(req);
             } else {
-                deferred.reject();
+                deferred.reject(req);
             }
         };
-
         return deferred.promise;
     }
 
-    function resolveUrls(urls) {
-        if (urls) {
-            var promises = [];
-            for (var prop in urls) {
-                if (urls.hasOwnProperty(prop)) {
-                    promises.push(getUrl(prop, urls[prop]));
-                }
-            }
-            return when.all(promises);
-        } else {
-            var deferred = when.defer();
-            deferred.resolve([]);
-            return deferred.promise;
-        }
-    }
-
-    function renderRouteMatch(match) {
-        resolveUrls(match.urls).then(
-            function (res) {
-                var props = {};
-                res.forEach(function (x) {
-                    props[x.prop] = JSON.parse(x.body);
-                });
-                renderComponent(match.get(props))
-            },
-            function () {
-                // TODO: Only render not found if all requests were 404.
-                renderNotFound();
-            }
-        );
-    };
-
     function renderPath(path) {
         var match = app.router.match(path);
-
         if (match) {
-            renderRouteMatch(match);
+            if (match.urls) {
+                var promises = [];
+                for (var prop in match.urls) {
+                    promises.push(when(getUrl(match.urls[prop]), function (req) {
+                        return {body: JSON.parse(req.responseText), prop: prop}
+                    }));
+                }
+                when.all(promises).then(function (res) {
+                    var props = {};
+                    res.forEach(function (x) { props[x.prop] = x.body; });
+                    renderComponent(match.get(props))
+                }, function () {
+                    // TODO: Only render not found if all requests were 404.
+                    renderNotFound();
+                });
+            } else {
+                renderComponent(match.get())
+            }
         } else {
             renderNotFound();
         }
